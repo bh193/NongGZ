c783e2c529a820cc40167e546c45464d5e9b522f
bawonyoli <bawonyoli@gmail.com>
2021-09-13 11:35:35 +0800

    新增 聯絡我們html、js資料夾、contact.js

---
+++ dev/js/contact.js
@@ -0,0 +1,16 @@
+function send(){
+    emailjs.send("service_9mcavrt","template_ymf7nzc",{
+        subject: "聯絡我們問題回報",
+        name: document.querySelector("#name").value,
+        option: document.querySelector("#qa").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+        }).then(response=>{
+            console.log("succes",response.status);
+        },(error)=>{
+            console.log(error);
+        })
+        
+}
+window.addEventListener("load", function(send) {
+  });
