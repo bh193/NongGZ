deb39cc76f5fc05032235a30cb16ff5f940798ce
bh193 <brianj193@gmail.com>
2021-09-23 21:21:52 +0800

    更新會員中心訂單區開合

---
+++ dev/js/activity3.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+
+    $(".preview a").on("click", function(){
+        $(".selected").removeClass("selected");
+        $(this).addClass("selected");
+        var picture = $(this).data();
+        
+        event.preventDefault(); //prevents page from reloading every time you click a thumbnail
+
+        $(".full img").fadeOut( 100, function() { 
+          $(".full img").attr("src", picture.full);
+          $(".full").attr("href", picture.full);
+          $(".full").attr("title", picture.title);
+
+      }).fadeIn();
+
+    });// end on click
+
+    $(".full").fancybox({
+        helpers : {
+            title: {
+                type: 'inside'
+            }
+        },
+        closeBtn : true,
+    });
+
+});//end doc ready
+
+/////////////////
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+/////////////
+
+
+
+$('.slider2').slick({
+
+dots: true,
+infinite: true,
+speed: 500,
+fade: true,
+cssEase: 'linear'
+});
--- dev/js/mem_farmer.js
+++ dev/js/mem_farmer.js
@@ -1,22 +1,21 @@
 
 
-    $(".item").click(function(){
-      $(this).next(".test").slideToggle(300)
+  //   $(".item").click(function(){
+  //     $(this).next(".test").slideToggle(300)
      
-     var icon =  $(this).children().children(".item #plus");
-       if( icon.css("display") == "none"){
-         icon.show();
-       }else{
-         icon.hide();
-       }
+  //    var icon =  $(this).children().children(".item #plus");
+  //      if( icon.css("display") == "none"){
+  //        icon.show();
+  //      }else{
+  //        icon.hide();
+  //      }
      
-       var icon =  $(this).children().children(".item #minus");
-       if( icon.css("display") == "none"){
-         icon.show();
-       }else{
-         icon.hide();
-       }
-   });
+  //      var icon =  $(this).children().children(".item #minus");
+  //      if( icon.css("display") == "none"){
+  //        icon.show();
+  //      }else{
+  //        icon.hide();
+  //      }
+  //  });
 
-
-   
+  
