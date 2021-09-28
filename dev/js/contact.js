f15cedbdc6cf96723db35f52ece982cd69d0faf1
bawonyoli <bawonyoli@gmail.com>
2021-09-29 00:33:08 +0800

    contact js html修改

--- dev/js/contact.js
+++ dev/js/contact.js
@@ -1,19 +1,18 @@
 function clearinput(){
-        document.getElementsByTagName("input").value="";
-        document.getElementsByTagName("selet").value="";
-        document.getElementsByTagName("textarea").value="";    
+    document.getElementById("contact").reset();
     }
-    function sendmail(){
-         emailjs.send("service_9mcavrt","template_ymf7nzc",{
-        subject: "[農果子]問題回報",
-        name: document.querySelector("#name").value,
-        option: document.querySelector("#qa").value,
-        email: document.querySelector("#email").value,
-        message: document.querySelector("#message").value,
-        }).then(response=>{
-            console.log("succes",response.status);
-        },(error)=>{
-            console.log(error);
-        })
-    }
+function sendmail(){
+    emailjs.send("service_9mcavrt","template_ymf7nzc",{
+    subject: "[農果子]問題回報",
+    name: document.querySelector("#name").value,
+    option: document.querySelector("#qa").value,
+    email: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
+    }).then(response=>{
+        console.log("succes",response.status);
+    },(error)=>{
+        console.log(error);
+    })
+}
+
 
