9834bd6b1bf763751e0c0fc9ad7540dd69a6e5d7
bh193 <brianj193@gmail.com>
2021-10-01 11:43:15 +0800

    後臺活動

--- dev/js/back_activity_info.js
+++ dev/js/back_activity_info.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
     $(this).parent().siblings(".item").find(".item_text").val("");
   });
 });
+
+
+$(function () {
+  $('.content').click(function () {
+      $(this).find('.con_text').slideToggle(500,"easeOutExpo");
+  }).children('.con_text').hide();
+
+  $('.content').click(function () {
+      $(this).find('.turn').toggleClass('turn_zero');
+  })
+});
