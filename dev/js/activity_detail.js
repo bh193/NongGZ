edc94800ccb4f941965cd5c1282dfa751b4d0676
bh193 <brianj193@gmail.com>
2021-10-04 20:09:28 +0800

    no message

--- dev/js/activity_detail.js
+++ dev/js/activity_detail.js
@@ -1,15 +1,16 @@
-
 $(document).ready(function(){
+  // created(){}
+  // mounted(){}
 
-    $(".preview a").on("click", function(){
+    $(".sm-pic").on("click", function(){
         $(".selected").removeClass("selected");
         $(this).addClass("selected");
         var picture = $(this).data();
         
         event.preventDefault(); //prevents page from reloading every time you click a thumbnail
 
-        $(".full img").fadeOut( 100, function() { 
-          $(".full img").attr("src", picture.full);
+        $(".first").fadeOut( 100, function() { 
+          $(".first").attr("src", picture.full);
           $(".full").attr("href", picture.full);
           $(".full").attr("title", picture.title);
 
