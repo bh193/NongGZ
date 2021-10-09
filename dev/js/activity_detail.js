7aebe8d87e9500a91e5cb0911764f8069ed2ec69
bh193 <brianj193@gmail.com>
2021-10-09 18:54:44 +0800

    會員中心初步連PHP

--- dev/js/activity_detail.js
+++ dev/js/activity_detail.js
@@ -1,62 +1,57 @@
-$(document).ready(function(){
-  // created(){}
-  // mounted(){}
+// $(document).ready(function(){
+//   // created(){}
+//   // mounted(){}
 
-    $(".sm-pic").on("click", function(){
-        $(".selected").removeClass("selected");
-        $(this).addClass("selected");
-        var picture = $(this).data();
+//     $(".sm-pic").on("click", function(){
+//         $(".selected").removeClass("selected");
+//         $(this).addClass("selected");
+//         var picture = $(this).data();
         
-        event.preventDefault(); //prevents page from reloading every time you click a thumbnail
+//         event.preventDefault(); //prevents page from reloading every time you click a thumbnail
 
-        $(".first").fadeOut( 100, function() { 
-          $(".first").attr("src", picture.full);
-          $(".full").attr("href", picture.full);
-          $(".full").attr("title", picture.title);
+//         $(".first").fadeOut( 100, function() { 
+//           $(".first").attr("src", picture.full);
+//           $(".full").attr("href", picture.full);
+//           $(".full").attr("title", picture.title);
 
-      }).fadeIn();
+//       }).fadeIn();
 
-    });// end on click
+//     });// end on click
 
-    $(".full").fancybox({
-        helpers : {
-            title: {
-                type: 'inside'
-            }
-        },
-        closeBtn : true,
-    });
+//     $(".full").fancybox({
+//         helpers : {
+//             title: {
+//                 type: 'inside'
+//             }
+//         },
+//         closeBtn : true,
+//     });
 
-});//end doc ready
+// });
+//end doc ready
 
 
 
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36251023-1']);
-_gaq.push(['_setDomainName', 'jqueryscript.net']);
-_gaq.push(['_trackPageview']);
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', 'UA-36251023-1']);
+// _gaq.push(['_setDomainName', 'jqueryscript.net']);
+// _gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+// (function() {
+//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+// })();
 
 /////////////
 
 
 
-$('.slider2').slick({
-// prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/svg/left-btn-g.svg' id='left-arrow'>",
-// nextArrow:"<img class='a-right control-c next slick-next' src='../images/svg/right-btn-g.svg' id='right-arrow'>",
-dots: true,
-infinite: true,
-speed: 500,
-fade: true,
-cssEase: 'linear'
-});
-
+// $('.preview').click(function () {
+//   $(this).find('.mask').hide();
+//   $(this).siblings().find('.mask').show();
+// });
 
 /////////// 彈跳視窗速度
 
@@ -66,13 +61,3 @@ $('a[href="#modal_order"]').click(function(event) {
       fadeDuration: 300
     });
   });
-
-  
-  
-
-
-$('.preview').click(function () {
-  $(this).find('.mask').hide();
-  $(this).siblings().find('.mask').show();
-});
-
--- dev/js/mem_center.js
+++ dev/js/mem_center.js
@@ -1,75 +1,75 @@
-// 認養及體驗活動訂單的頁面切換
+// // 認養及體驗活動訂單的頁面切換
 
-$(function(){
-    var $orderSelect = $('.order-select') ,
-        $tabs = $orderSelect.find('.tabs') ,
-        $page = $tabs.find('a') ,
-        $order = $orderSelect.find('.orders') ,
-        $content = $order.find('> li');
+// $(function(){
+//     var $orderSelect = $('.order-select') ,
+//         $tabs = $orderSelect.find('.tabs') ,
+//         $page = $tabs.find('a') ,
+//         $order = $orderSelect.find('.orders') ,
+//         $content = $order.find('> li');
     
-    $page.eq(0).addClass('active');
-    $content.eq(0).show();
+//     $page.eq(0).addClass('active');
+//     $content.eq(0).show();
     
-    $page.on('click',function(){
-        var $pageIndex = $(this).index();
-        $(this).addClass('active').siblings().removeClass('active');
-        $content.eq($pageIndex).show().siblings().hide();
+//     $page.on('click',function(){
+//         var $pageIndex = $(this).index();
+//         $(this).addClass('active').siblings().removeClass('active');
+//         $content.eq($pageIndex).show().siblings().hide();
 
-        // if( $(this).find(".tree_green").css("display") == "none"){
-        //   $(this).find(".tree_green").show();
-        // }else{
-        //   $(this).find(".tree_green").hide();
-        // }
+//         // if( $(this).find(".tree_green").css("display") == "none"){
+//         //   $(this).find(".tree_green").show();
+//         // }else{
+//         //   $(this).find(".tree_green").hide();
+//         // }
        
-        // $(this).find(".tree_gray").css("display", "none");
+//         // $(this).find(".tree_gray").css("display", "none");
 
-    });
-});
-$(".select_1").click(function(){
-  $(this).find(".tree_gray").hide();
-  $(this).find(".tree_green").show();
-  $(this).siblings(".select_2").find(".apple_gray").show();
-  $(this).siblings(".select_2").find(".apple_green").hide();
-  $(this).siblings(".select_3").find(".coupon_gray").show();
-  $(this).siblings(".select_3").find(".coupon_green").hide();
-});
+//     });
+// });
+// $(".select_1").click(function(){
+//   $(this).find(".tree_gray").hide();
+//   $(this).find(".tree_green").show();
+//   $(this).siblings(".select_2").find(".apple_gray").show();
+//   $(this).siblings(".select_2").find(".apple_green").hide();
+//   $(this).siblings(".select_3").find(".coupon_gray").show();
+//   $(this).siblings(".select_3").find(".coupon_green").hide();
+// });
 
-$(".select_2").click(function(){
-  $(this).find(".apple_gray").hide();
-  $(this).find(".apple_green").show();
-  $(this).siblings(".select_1").find(".tree_gray").show();
-  $(this).siblings(".select_1").find(".tree_green").hide();
-  $(this).siblings(".select_3").find(".coupon_gray").show();
-  $(this).siblings(".select_3").find(".coupon_green").hide();
-});
+// $(".select_2").click(function(){
+//   $(this).find(".apple_gray").hide();
+//   $(this).find(".apple_green").show();
+//   $(this).siblings(".select_1").find(".tree_gray").show();
+//   $(this).siblings(".select_1").find(".tree_green").hide();
+//   $(this).siblings(".select_3").find(".coupon_gray").show();
+//   $(this).siblings(".select_3").find(".coupon_green").hide();
+// });
 
-$(".select_3").click(function(){
-  $(this).find(".coupon_gray").hide();
-  $(this).find(".coupon_green").show();
-  $(this).siblings(".select_1").find(".tree_gray").show();
-  $(this).siblings(".select_1").find(".tree_green").hide();
-  $(this).siblings(".select_2").find(".apple_gray").show();
-  $(this).siblings(".select_2").find(".apple_green").hide();
-});
+// $(".select_3").click(function(){
+//   $(this).find(".coupon_gray").hide();
+//   $(this).find(".coupon_green").show();
+//   $(this).siblings(".select_1").find(".tree_gray").show();
+//   $(this).siblings(".select_1").find(".tree_green").hide();
+//   $(this).siblings(".select_2").find(".apple_gray").show();
+//   $(this).siblings(".select_2").find(".apple_green").hide();
+// });
 
-// 單筆訂單的開合跟icon切換
+// // 單筆訂單的開合跟icon切換
 
-    $(".item").click(function(){
-      $(this).next(".detail").slideToggle()
+//     $(".item").click(function(){
+//       $(this).next(".detail").slideToggle()
      
-     var icon =  $(this).children().children(".item #plus");
-       if( icon.css("display") == "none"){
-         icon.show();
-       }else{
-         icon.hide();
-       }
+//      var icon =  $(this).children().children(".item #plus");
+//        if( icon.css("display") == "none"){
+//          icon.show();
+//        }else{
+//          icon.hide();
+//        }
      
-       var icon =  $(this).children().children(".item #minus");
-       if( icon.css("display") == "none"){
-         icon.show();
-       }else{
-         icon.hide();
-       }
-   });
+//        var icon =  $(this).children().children(".item #minus");
+//        if( icon.css("display") == "none"){
+//          icon.show();
+//        }else{
+//          icon.hide();
+//        }
+//    });
 
    
