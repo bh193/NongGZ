a53a251237ea0635240588c4b477aea593796276
bernice-chen <bernice19851206@gmail.com>
2021-10-10 19:54:54 +0800

    增加 php

--- dev/js/post.js
+++ dev/js/post.js
@@ -7,7 +7,12 @@ let app = new Vue({
         images:'',
         selectedFarm:'',
         posttxt:'',
-        filterFarm:''
+        filterFarm:'',
+        selectNew:'1',
+        options: [
+            { text:"最新的", value: '1' },
+            { text:"熱門的", value: '2' },
+            ],
     },
     methods: {
         imgchange(e){
@@ -21,16 +26,12 @@ let app = new Vue({
         },
         postdata(){
             console.log('postdata2');
-            // let xhr = new XMLHttpRequest();
-            // xhr.open("post", "../dist/phps/returnPosts.php", true);
-            // xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
             // //送出資料
             let data_info = {
                 'farm_id': this.selectedFarm, 
                 'post_img':this.images,
                 'post_content':this.posttxt,
             };
-            // xhr.send(data_info);
             $.ajax({
                 type: 'post',
                 url: "../dist/phps/returnPosts.php",
@@ -53,7 +54,6 @@ let app = new Vue({
         // }
     },
     computed:{
-        //搜尋農場關鍵字
         filterRow() {
             // prodRows2 = prodRows;
             return this.prodRows.filter(prodRow =>{return prodRow.farm_name.toLowerCase().includes(this.filterFarm.toLowerCase())});
@@ -80,24 +80,44 @@ function getFarms() {
     xhr.open("get", "../dist/phps/getFarms.php",true);
     xhr.send(null);
 }
+//最新|熱門排序
+function selectNd() {
+    if("selectNew.${value}" == 2){
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            app.farms = JSON.parse(xhr.responseText)
+        }
+        xhr.open("get", "../dist/phps/getPosts_2.php",true);
+        xhr.send(null);
+    }else{
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            app.farms = JSON.parse(xhr.responseText)
+        }
+        xhr.open("get", "../dist/phps/getPosts.php",true);
+        xhr.send(null);
+    }
+}
 
 function switchFavorite(){
-    if(heart.src == "../images/post/heart_gray.svg"){
-        heart.src = "../images/post/heart_red.svg";
+    if(heart.src == "../dist/images/post/heart_gray.svg"){
+        heart.src = "../dist/images/post/heart_red.svg";
     }else{
-        heart.src = "../images/post/heart_gray.svg";
+        heart.src = "../dist/images/post/heart_gray.svg";
     }
 }
 let heart = document.getElementsByClassName("btn_heart");
 heart.onclick = switchFavorite;
 
 window.addEventListener("load", function(){
-    //---------------------網頁的初始設定
+    
+    selectNd();//---------------------網頁的初始設定
     getProducts();
 
     getFarms();
 
     switchFavorite();
+
  
 });
 
--- dev/phps/returnPosts.php
+++ dev/phps/returnPosts.php
@@ -4,17 +4,14 @@
         //引入連線工作的檔案
         require_once("./connectNGZ.php");
 
-        //檢查是否有images資料夾
         $dir = "../images/post";
         if(file_exists($dir) == false){
             mkdir($dir);
         }
-
         $data = $_POST["post_img"];
-        
         if (preg_match('/^data:image\/(\w+);base64,/', $data, $type)) {
             $data = substr($data, strpos($data, ',') + 1);
-            $type = strtolower($type[1]); // jpg, png, gif
+            $type = strtolower($type[1]);
         
             if (!in_array($type, [ 'jpg', 'jpeg', 'gif', 'png' ])) {
                 throw new \Exception('invalid image type');
@@ -30,23 +27,25 @@
         }
         $dname = uniqid();
         $today = date('Y-m-d');
-        $putFile = file_put_contents("$dir/"."$dname".".{$type}", $data);
+        file_put_contents("$dir/"."$dname".".{$type}", $data);
+        $putFile = "$dname".".{$type}";
         $memid = "1";
-        
+
         // 執行sql指令並取得pdoStatement
         $sql = "insert into post(post_date, post_img, post_content, farm_id, mem_id)
-        values($today, :post_img, :post_content, :farm_id, :mem_id)";
-        $postf = $pdo->prepare($sql);
-        $postf -> bindValue(":post_img", $putFile);
-        $postf -> bindValue(":post_content", $_POST["post_content"]);
-        $postf -> bindValue(":farm_id", $_POST["farm_id"]);
-        $postf -> bindValue(":mem_id", $memid);
-        $postf -> exceute();
+        values(:post_date, :post_img, :post_content, :farm_id, :mem_id)";
+        $post = $pdo->prepare($sql);
+        $post -> bindValue(":post_img", $putFile);
+        $post -> bindValue(":post_content", $_POST["post_content"]);
+        $post -> bindValue(":farm_id", $_POST["farm_id"]);
+        $post -> bindValue(":post_date", $today);
+        $post -> bindValue(":mem_id", $memid);
+        $post -> execute();
         echo "異動成功";
     } catch (PDOException $e) {
         $pdo->rollBack();
         $errMsg .= "錯誤原因 : ".$e -> getMessage(). "<br>";
         $errMsg .= "錯誤行號 : ".$e -> getLine(). "<br>";	
         echo $errMsg;
-        }
+    }
 ?>
