4f808d7298505616d842a4b25d6fd8fb5c89c633
bernice-chen <bernice19851206@gmail.com>
2021-10-14 11:14:38 +0800

    add getFarmactivity php

--- dev/phps/getFarmactivity.php
+++ dev/phps/getFarmactivity.php
@@ -5,10 +5,9 @@
         require_once("./connectNGZ.php");
         
         //執行sql指令並取得pdoStatement
-        $sql = "SELECT p.post_img, p.post_content, m.mem_img, f.farm_name, f.farm_banner, p.post_feedback
-        from post p join member m on (p.mem_id = m.mem_id)
-             join farm f on (p.farm_id = f.farm_id)
-		where f.farm_id = 1";
+        $sql = "SELECT * from activity a join farm f on a.farm_id = f.farm_id
+                         join city c on f.city_id = c.city_id
+                         order by a.farm_id";
         $products = $pdo->query($sql);
         $prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($prodRows);
