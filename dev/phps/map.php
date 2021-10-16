40c87531ced5e5becbf145d512e87955e2606d24
bawonyoli <bawonyoli@gmail.com>
2021-10-16 09:09:37 +0800

    修改找農場吧串接資料

---
+++ dev/phps/map.php
@@ -0,0 +1,15 @@
+<?php
+    try{
+        //引入連線工作的檔案
+        require_once("./connecttbame.php");
+        //執行sql指令並取得pdoStatement
+        $sql = "select farm_name, farm_id,farm_address,farm_imgA,farm_imgB from farm";
+        $farm = $pdo->query($sql);
+        $farmRows = $farm->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($farmRows);
+    } catch (Exception $e) {
+        echo "錯誤行號 : ", $e->getLine(), "<br>";
+        echo "錯誤原因 : ", $e->getMessage(), "<br>";
+        //echo "系統暫時不能正常運行，請稍後再試<br>";	
+    }
+?>
