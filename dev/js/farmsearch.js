16eaf523d92cda4ef2044eba6835f6928ed5a67b
bawonyoli <bawonyoli@gmail.com>
2021-10-22 14:07:07 +0800

    來農場吧修改

--- dev/js/farmsearch.js
+++ dev/js/farmsearch.js
@@ -87,12 +87,13 @@ Vue.component('mapList',{
             };
             this.map = new google.maps.Map(document.getElementById('map'), {
               center: location,
-              zoom: 10,
+              zoom: loc.zoom,
               mapTypeId: 'roadmap',
               mapTypeControl: false,
               streetViewControl: false,
               fullscreenControl: false,
             });
+            
 
          
           this.farm.forEach(r => {
@@ -140,199 +141,11 @@ Vue.component('mapList',{
             });
         
           this.$nextTick(this.setSwiper)//呼叫setSwiper()
-      },
-      option1(){
-        let location = {   //預設中心點位置
-          lat: 25.04541, // 經度
-          lng: 121.54781 // 緯度
-          
-          
-        };
-        this.map = new google.maps.Map(document.getElementById('map'), {
-          center: location,
-          zoom: 13,
-          mapTypeId: 'roadmap',
-          mapTypeControl: false,
-          streetViewControl: false,
-          fullscreenControl: false,
-        });
-
-      axios.get('./phps/map.php')
-      .then(res => { 
-      this.farmRow=res.data;
-      Array.prototype.forEach.call(this.farmRow, r => {
-      let latLng = new google.maps.LatLng(r.farm_lat,r.farm_lon);  
-      let marker = new google.maps.Marker({
-            position: latLng,
-            map: this.map,
-            animation: google.maps.Animation.DROP,
-            
-          });
-          // info window
-
-          let infowindow = new google.maps.InfoWindow({
-            
-            content: `<div class="col">
-            <div class="img" style="background-image: url('./images/farm/${r.farm_imgA}');"></div>
-            <div class="info">
-                <h3>${r.farm_name}</h3>
-                <p>地址：<br>${r.farm_address}</p>
-                <div class="btn">
-                <button onclick="window.location.href='farminfon.html?farm_id=${r.farm_id}'">農場詳情</button>
-                  </div>
-                </div>
-            </div>
-        </div>` 
-          });  
-         
-        
-          //  滑鼠移過第二個視窗，第一個會關閉
-          marker.addListener('mouseover', e => {
-            if (this.infowindow) this.infowindow.close();
-            infowindow.open(this.map, marker);
-            this.infowindow = infowindow;
-          });
-
-
-
-
-        });
-    
-      this.$nextTick(this.setSwiper)//呼叫setSwiper()
-      
-  });
-      },
-      option2(){
-        let location = {   //預設中心點位置
-          lat: 24.995186, // 經度
-          lng: 121.478812 // 緯度
-          
-          
-        };
-        this.map = new google.maps.Map(document.getElementById('map'), {
-          center: location,
-          zoom: 13,
-          mapTypeId: 'roadmap',
-          mapTypeControl: false,
-          streetViewControl: false,
-          fullscreenControl: false,
-        });
-
-      axios.get('./phps/map.php')
-      .then(res => { 
-      this.farmRow=res.data;
-      Array.prototype.forEach.call(this.farmRow, r => {
-      let latLng = new google.maps.LatLng(r.farm_lat,r.farm_lon);  
-      let marker = new google.maps.Marker({
-            position: latLng,
-            map: this.map,
-            animation: google.maps.Animation.DROP,
-            
-          });
-          // info window
-
-          let infowindow = new google.maps.InfoWindow({
-            
-            content: `<div class="col">
-            <div class="img" style="background-image: url('./images/farm/${r.farm_imgA}');"></div>
-            <div class="info">
-                <h3>${r.farm_name}</h3>
-                <p>地址：<br>${r.farm_address}</p>
-                <div class="btn">
-                <button onclick="window.location.href='farminfon.html?farm_id=${r.farm_id}'">農場詳情</button>
-                  </div>
-                </div>
-            </div>
-        </div>` 
-          });  
-         
-        
-          //  滑鼠移過第二個視窗，第一個會關閉
-          marker.addListener('mouseover', e => {
-            if (this.infowindow) this.infowindow.close();
-            infowindow.open(this.map, marker);
-            this.infowindow = infowindow;
-          });
-
-
-
-
-        });
-    
-      this.$nextTick(this.setSwiper)//呼叫setSwiper()
-      
-  });
-      },
-      option3(){
-        let location = {   //預設中心點位置
-          lat: 24.852503, // 經度
-          lng: 121.271133 // 緯度
-          
-          
-        };
-        this.map = new google.maps.Map(document.getElementById('map'), {
-          center: location,
-          zoom: 12,
-          mapTypeId: 'roadmap',
-          mapTypeControl: false,
-          streetViewControl: false,
-          fullscreenControl: false,
-        });
-
-      axios.get('./phps/map.php')
-      .then(res => { 
-      this.farmRow=res.data;
-      Array.prototype.forEach.call(this.farmRow, r => {
-      let latLng = new google.maps.LatLng(r.farm_lat,r.farm_lon);  
-      let marker = new google.maps.Marker({
-            position: latLng,
-            map: this.map,
-            animation: google.maps.Animation.DROP,
-            
-          });
-          // info window
-
-          let infowindow = new google.maps.InfoWindow({
-            
-            content: `<div class="col">
-            <div class="img" style="background-image: url('./images/farm/${r.farm_imgA}');"></div>
-            <div class="info">
-                <h3>${r.farm_name}</h3>
-                <p>地址：<br>${r.farm_address}</p>
-                <div class="btn">
-                <button onclick="window.location.href='farminfon.html?farm_id=${r.farm_id}'">農場詳情</button>
-                  </div>
-                </div>
-            </div>
-        </div>` 
-          });  
-         
-        
-          //  滑鼠移過第二個視窗，第一個會關閉
-          marker.addListener('mouseover', e => {
-            if (this.infowindow) this.infowindow.close();
-            infowindow.open(this.map, marker);
-            this.infowindow = infowindow;
-          });
-
-
-
-
-        });
-    
-      this.$nextTick(this.setSwiper)//呼叫setSwiper()
-      
-  });
-      },
-    
-
-
-
-      
+      },       
       setSwiper() {
           this.swiper = new Swiper(".mySwiper", {
             slidesPerView: 3.5,
-            spaceBetween: 30,
+            spaceBetween: 25,
             freeMode: true,
             // pagination: {
             //   el: ".swiper-pagination",
@@ -346,7 +159,6 @@ Vue.component('mapList',{
     .then(res => { 
     this.farmRow=res.data;
     this.initMap();
-    this.citybar();
    
     
 });
